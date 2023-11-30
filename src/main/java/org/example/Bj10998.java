fb2299753ea247907c757ce82245bd0ffa35bc38
dongbin <ornornro@gmail.com>
2023-11-30 18:06:34 +0900

    BaekJoon

---
+++ src/main/java/org/example/Bj10998.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Bj10998 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
